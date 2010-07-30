d6698cfbb38b010c38b6df464638f3a7201fe65c
jon <jon@9b148242-5f64-11df-906c-310fe9e996a1>
2010-07-30 12:47:41 +0000

    x
    git-svn-id: file:///srv/old-firewall-data/svn/jmc/docs@19 9b148242-5f64-11df-906c-310fe9e996a1

---
+++ hackipedia/software/huedit/wcwidvf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <wchar.h> /* for wcwidth() */
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+int main(int argc,char **argv) {
+	wchar_t c;
+
+	return 0;
+}
+
