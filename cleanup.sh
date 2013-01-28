61da1ed42ee8a7cc8383d9fdd20fbc31c5913c1b
jon <jon@9b148242-5f64-11df-906c-310fe9e996a1>
2013-01-28 12:07:53 +0000

    x
    git-svn-id: file:///srv/old-firewall-data/svn/jmc/docs@80 9b148242-5f64-11df-906c-310fe9e996a1

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,6 @@
 find -name \*~ -delete
 find -name \*~ -exec svn del {} +
 find -name \*.err -delete
-svn del hackipedia/software/huedit/huedit hackipedia/software/huedit/wcwidgen
+svn del hackipedia/software/huedit/huedit hackipedia/software/huedit/wcwidgen hackipedia/software/huedit/wcwidvf
 find -name \*.o -exec svn del {} +
 
