8b571659983198fdec90a5f7e7933e4bc477c29c
Jonathan Campbell <jonathan@castus.tv>
2017-05-23 11:35:41 -0700

    cleanup

--- cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-find -name \*~ -delete
-find -name \*~ -exec svn del {} +
-find -name \*.err -delete
-for i in hackipedia/software/huedit/huedit hackipedia/software/huedit/wcwidgen hackipedia/software/huedit/wcwidvf; do svn del $i --force; done
-find -name \*.o -exec svn del {} +
-find -name \*.tmp -exec rm -Rfv {} +
-
--- env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export TEXLIVE=/usr/texlive/bin/i386-linux
-export PATH=$TEXLIVE:$PATH
-
